dbfc36c8191f64840ac73ffc48f6e587284b9126
Ashish4chauhan <148565101+Ashish4chauhan@users.noreply.github.com>
2026-01-10 22:02:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 /* ---------------- DATABASE ---------------- */
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST || process.env.MYSQLHOST,
+  user: process.env.DB_USER || process.env.MYSQLUSER,
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+  port: process.env.DB_PORT || process.env.MYSQLPORT || 3306
 });
 
 db.connect(err => {
